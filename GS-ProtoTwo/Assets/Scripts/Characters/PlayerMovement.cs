934e177b5dc9da26ad476dff8cb644d809f19662
Jack Belton <jack.belton@hotmail.com>
2020-08-05 17:25:19 +1200

    Playerwalk Anims + Player movement cleanup

--- GS-ProtoTwo/Assets/Scripts/Characters/PlayerMovement.cs
+++ GS-ProtoTwo/Assets/Scripts/Characters/PlayerMovement.cs
@@ -8,14 +8,18 @@ public class PlayerMovement : MonoBehaviour
     [Range(0.0f, 0.25f), Tooltip("0.0f == Off")]
     public float MoveSmoothing = 0.1f;
 
+    public float CheckToGroundDistance = 1.0f;
     public float GroundPoundForce = 50.0f;
     public bool SpideyPowers = true;
     private Vector3 velocity;
 
     private Rigidbody rb;
+    private Animator playerAnims;
     // Start is called before the first frame update
     void Start()
     {
+        playerAnims = GetComponentInChildren<Animator>();
+
         rb = GetComponent<Rigidbody>();
     }
 
@@ -25,15 +29,39 @@ public class PlayerMovement : MonoBehaviour
         MovePlayer();
     }
 
+
+    private void FixedUpdate()
+    {
+        RaycastHit raycastHit;
+
+        if (Physics.Raycast(transform.position, -transform.up, out raycastHit, Mathf.Infinity ))
+        {
+            if ((raycastHit.distance > CheckToGroundDistance))
+            {
+                SpideyPowers = true;
+            }
+            else
+            {
+                SpideyPowers = false;
+            }
+        }
+    }
     void MovePlayer()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Vector3 moveVec = (((transform.forward) * vertical)
-            + ((transform.right) * horizontal)) * PlayerSpeed;
-        
+        Vector3 moveVec = Vector3.Normalize((((transform.forward) * vertical)
+            + ((transform.right) * horizontal))) * PlayerSpeed;
 
+        if (moveVec != Vector3.zero)
+        {
+            playerAnims.SetBool("Walk", true);
+        }
+        else
+        {
+            playerAnims.SetBool("Walk", false);
+        }
         rb.AddForce(moveVec);
         if (SpideyPowers)
         {
@@ -49,20 +77,20 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.transform.tag == "Ground")
-        {
-            SpideyPowers = false;
-        }
+        //if (collision.transform.tag == "Ground")
+        //{
+        //    SpideyPowers = false;
+        //}
 
         GetComponent<EncounterStart>().EncounterOnCollisionEnter(collision);
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        if (collision.transform.tag == "Ground")
-        {
-            SpideyPowers = true;
-        }
+        //if (collision.transform.tag == "Ground")
+        //{
+        //    SpideyPowers = true;
+        //}
     }
 
 }
